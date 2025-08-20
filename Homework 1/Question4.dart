bf03b38c6665734f974a552c22cf0460cb0388ac
Mahdi salah <saaess33@gmail.com>
2025-08-20 23:38:53 +0300

    Re-upload files

---
+++ Homework 1/Question4.dart
@@ -0,0 +1,8 @@
+//Q4/ Create two string variables: one with your
+// first name and one with your last name.
+
+void main() {
+  String firstName = "Mahdi";
+  String lastName = "Salah";
+  print("$firstName $lastName");
+}
---
+++ Homework 1/Question7.dart
@@ -0,0 +1,11 @@
+//Create a dynamic variable
+// and assign it a string value.
+// Then assign it a number. Print
+//the variable after each assignment.
+void main() {
+  dynamic take;
+  take = "Mahdi";
+  print("$take");
+  take = 21;
+  print("$take");
+}
